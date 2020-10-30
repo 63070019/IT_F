56988aa13e63601af83646c31e23e992ebf0329e
63070019 <73679704+63070019@users.noreply.github.com>
2020-10-30 11:06:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,18 +3,22 @@
         <title>Hello Fat World</title>
     </head>
     <body>
-        <h1>My First Heading</h1>
-        <p>My First Paragharp</p>
+        <h1>เรื่องตลก5บรรทัด?</h1>
+        <p>นาย จิรพัฒน์ วุ่นพ้วน(เดี่ยว)</p>
+        <p>คือแบบว่านะครับ</p>
+        <p>เรื่องตลกที่จะเขียนนี่..มันอยุ่ข้างล่างแมวนะครับ</p>
         <img src="Fat_cat.jpg">
         <img src="Meme_cat.jpg">
         <br>
         <a href="http://www.it.kmitl.ac.th">This is a Link</a>
 
-        <h2>The Unordered list</h2>
+        <h2>เรื่องมันมีอยู่ว่า</h2>
         <ul>
-            <li>Google</li>
-            <li>Yahoo</li>
-            <li>Amazon</li>
+            <li>คือแบบผมว่ามันตลกมากเลยนะครับ</li>
+            <li>ผมนั่งคิดอยู่นั่น</li>
+            <li>ผมนึกขึ้นมาได้</li>
+            <li>ผมนึกไม่ออกครับ....</li>
+            <li>...ขอบคุณครับ   :p</li>
         </ul>
     </body>
 </html>
