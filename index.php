b133f9e9ec3d877761a0201905d2c177f68ff390
63070019 <73679704+63070019@users.noreply.github.com>
2020-10-30 10:54:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
-<?php
-echo "<h2>IT@KMITL</h2>";
-echo "Hello IT<br>";
-?>
+<html>
+    <head>
+        <title>Hello Fat World</title>
+    </head>
+    <body>
+        <h1>My First Heading</h1>
+        <p>My First Paragharp</p>
+        <img src="Fat_cat.jpg"
+        <br>
+        <a href="http://www.it.kmitl.ac.th">This is a Link</a>
+
+        <h2>The Unordered list</h2>
+        <ul>
+            <li>Google</li>
+            <li>Yahoo</li>
+            <li>Amazon</li>
+        </ul>
+    </body>
+</html>
